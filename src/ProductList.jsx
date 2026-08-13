fe18fbdd5159a21482d618b947c913c4b2de0285
elaeset <elaeset@yahoo.com>
2026-08-12 23:58:56 -0400

    task4

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -6,6 +6,9 @@ function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const calculateTotalQuantity = () => {
+        return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+    };
 
     const plantsArray = [
         {
@@ -214,7 +217,7 @@ function ProductList({ onHomeClick }) {
             ]
         }
     ];
-    
+
     const handleAddToCart = (product) => {
         dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
       
